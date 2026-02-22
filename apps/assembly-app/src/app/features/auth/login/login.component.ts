2ac9c729d62ea473ce3f2a5dcb4d9167b6a267aa
codingfer <codingfer@gmail.com>
2026-02-22 14:34:56 -0400

    add a basic login, we need review the card and this works correctly

--- apps/assembly-app/src/app/features/auth/login/login.component.ts
+++ apps/assembly-app/src/app/features/auth/login/login.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {AuthService} from "../../../core/guards/auth.guard";
+import {CardComponent} from "@assembly/assembly-ui";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [
+    CardComponent,
+    FormsModule
+  ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+
+  email = '';
+  password = '';
   constructor(private auth: AuthService, private router: Router) {}
 
   onLogin() {
     this.auth.login('demo-token');
     this.router.navigate(['/dashboard']);
+  }
+
+  onSubmit() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // Aquí llamarías al AuthService para hacer login
   }
 }
